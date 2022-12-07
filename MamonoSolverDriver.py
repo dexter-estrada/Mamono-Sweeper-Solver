0654f5472cc55d80d62b4060c0262202ecd8461f
Dexter Estrada <dexter.estrada99@gmail.com>
2022-12-06 23:13:42 -0800

    Now simulates 100 games

--- MamonoSolverDriver.py
+++ MamonoSolverDriver.py
@@ -5,12 +5,20 @@ from MamonoSolverRand import MamonoSolverRand
 
 def main():
     mamonoGameInstance = MamonoSweeper()
-    #mamonoSolverInstance = MamonoSolver(mamonoGameInstance)
-    mamonoSolverInstance2 = MamonoSolverRand(mamonoGameInstance)
+    mamonoSolverInstance = MamonoSolver(mamonoGameInstance)
+    mamonoSolverRandom = MamonoSolverRand(mamonoGameInstance)
 
-    #mamonoSolverInstance.printBoards()
-    mamonoSolverInstance2.printBoards()
+    mamonoSolverInstance.printBoards()
+    mamonoSolverRandom.printBoards()
 
-
+    gamesWon = 0
+    # Checks the win percentage of the board
+    for i in range(0, 100):
+        mamonoGameTemp = MamonoSweeper()
+        mamonoSolverTemp = MamonoSolver(mamonoGameTemp)
+        #if mamonoSolverTemp.checkWin():
+            #gamesWon += 0
+    print("Winrate: ", gamesWon, "/", 100)
+    
 if __name__ == "__main__":
     main()
